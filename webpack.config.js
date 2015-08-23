58dba511f18735f396ec70ff96981d41cda1590d
Kuzya <alexkuz@gmail.com>
2015-08-23 15:02:19 +0300

    build demo

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,35 @@
 var path = require('path');
 var webpack = require('webpack');
 
+var isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = {
   devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './src/demo/index'
-  ],
+  entry: isProduction ?
+    [ './src/index' ] :
+    [
+      'webpack-dev-server/client?http://localhost:3000',
+      'webpack/hot/only-dev-server',
+      './src/index'
+    ],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'static'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: isProduction ? 'static/' : '/static/'
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
+  plugins:  isProduction ?
+    [new webpack.NoErrorsPlugin()] :
+    [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoErrorsPlugin()
+    ],
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
   module: {
     loaders: [{
       test: /\.jsx?$/,
-      loaders: ['react-hot', 'babel'],
+      loaders: isProduction ? ['babel'] : ['react-hot', 'babel'],
       include: path.join(__dirname, 'src'),
       exclude: path.join(__dirname, 'src', 'demo', 'FormattedText.jsx')
     }, {
